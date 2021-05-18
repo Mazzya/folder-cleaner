94daa8cfa03b385b6f60d25d0e83eabe04e891ec
Mazzya <yahyamazouarenna@gmail.com>
2021-05-18 22:34:22 +0200

    argparse added
    -p and --path options added
    -h and --help options added

--- app/cleaner.py
+++ app/cleaner.py
@@ -1,9 +1,16 @@
-import sys, os, shutil
+import sys
+import os
+import shutil
+import argparse
+
+parser = argparse.ArgumentParser(description="Clean folders quickly")
+parser.add_argument('-p', '--path', type=str, metavar='', required=True, help="Folder path to clean")
+args = parser.parse_args()
+
 
 class Core:
     def __init__(self, path):
         """ Constructor """
-        super().__init__()
         self.cleanFolder(path)
     
     def cleanFolder(self, path):
@@ -13,15 +20,13 @@ class Core:
                 fileNamePath = os.path.join(path + "//", fileName)
                 if os.path.isfile(fileNamePath):
                     os.unlink(fileNamePath)
+                    print(f"File: {fileName} cleaned")
                 else:
                     shutil.rmtree(fileNamePath)
-            print("Folder cleaned")
+            print("Folder cleaned successfully")
         except FileNotFoundError:
             print("The specified path could not be found")
 
 if __name__ == "__main__":
-    if len(sys.argv) == 2:
-        a = Core(sys.argv[1])
-    else:
-        print("Remember that you can only enter one argument and it must be a valid directory")
+    cleaner = Core(args.path)
     
